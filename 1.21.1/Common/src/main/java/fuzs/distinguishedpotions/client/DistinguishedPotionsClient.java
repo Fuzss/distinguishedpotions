c03fdbe4fbce94a475ce76565db5ed2d17648474
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-12-27 00:42:36 +0100

    apply potion mode overrides in code

--- 1.21.1/Common/src/main/java/fuzs/distinguishedpotions/client/DistinguishedPotionsClient.java
+++ 1.21.1/Common/src/main/java/fuzs/distinguishedpotions/client/DistinguishedPotionsClient.java
@@ -2,15 +2,14 @@ package fuzs.distinguishedpotions.client;
 
 import fuzs.distinguishedpotions.DistinguishedPotions;
 import fuzs.distinguishedpotions.client.handler.PotionDecorationsHandler;
+import fuzs.distinguishedpotions.client.handler.PotionItemModelHandler;
 import fuzs.distinguishedpotions.client.handler.PotionNameHandler;
 import fuzs.distinguishedpotions.config.ClientConfig;
-import fuzs.distinguishedpotions.data.client.DynamicModelProvider;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
+import fuzs.puzzleslib.api.client.core.v1.context.AdditionalModelsContext;
 import fuzs.puzzleslib.api.client.core.v1.context.ItemDecorationContext;
 import fuzs.puzzleslib.api.client.core.v1.context.ItemModelPropertiesContext;
-import fuzs.puzzleslib.api.core.v1.context.PackRepositorySourcesContext;
-import fuzs.puzzleslib.api.resources.v1.DynamicPackResources;
-import fuzs.puzzleslib.api.resources.v1.PackResourcesHelper;
+import fuzs.puzzleslib.api.client.event.v1.ModelEvents;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -25,17 +24,41 @@ public class DistinguishedPotionsClient implements ClientModConstructor {
     public static final ResourceLocation ITEM_MODEL_PROPERTY_LONG = DistinguishedPotions.id("long");
 
     @Override
+    public void onConstructMod() {
+        registerEventHandlers();
+    }
+
+    private static void registerEventHandlers() {
+        ModelEvents.MODIFY_UNBAKED_MODEL.register(PotionItemModelHandler::onModifyUnbakedModel);
+    }
+
+    @Override
+    public void onRegisterAdditionalModels(AdditionalModelsContext context) {
+        PotionItemModelHandler.getAllOverrideModelLocations().forEach(context::registerAdditionalModel);
+    }
+
+    @Override
     public void onRegisterItemModelProperties(ItemModelPropertiesContext context) {
-        context.registerItemProperty(ITEM_MODEL_PROPERTY_STRONG, (ItemStack itemStack, ClientLevel clientLevel, LivingEntity livingEntity, int i) -> {
-            if (!DistinguishedPotions.CONFIG.get(ClientConfig.class).dedicatedPotionBottles) return 0.0F;
-            Potion potion = PotionNameHandler.getPotion(itemStack);
-            return DistinguishedPotions.CONFIG.get(ClientConfig.class).strongPotions.contains(potion) ? 1.0F : 0.0F;
-        }, Items.POTION, Items.SPLASH_POTION, Items.LINGERING_POTION);
-        context.registerItemProperty(ITEM_MODEL_PROPERTY_LONG, (ItemStack itemStack, ClientLevel clientLevel, LivingEntity livingEntity, int i) -> {
-            if (!DistinguishedPotions.CONFIG.get(ClientConfig.class).dedicatedPotionBottles) return 0.0F;
-            Potion potion = PotionNameHandler.getPotion(itemStack);
-            return DistinguishedPotions.CONFIG.get(ClientConfig.class).longPotions.contains(potion) ? 1.0F : 0.0F;
-        }, Items.POTION, Items.SPLASH_POTION, Items.LINGERING_POTION);
+        context.registerItemProperty(ITEM_MODEL_PROPERTY_STRONG,
+                (ItemStack itemStack, ClientLevel clientLevel, LivingEntity livingEntity, int i) -> {
+                    if (!DistinguishedPotions.CONFIG.get(ClientConfig.class).dedicatedPotionBottles) return 0.0F;
+                    Potion potion = PotionNameHandler.getPotion(itemStack);
+                    return DistinguishedPotions.CONFIG.get(ClientConfig.class).strongPotions.contains(potion) ? 1.0F :
+                            0.0F;
+                },
+                Items.POTION,
+                Items.SPLASH_POTION,
+                Items.LINGERING_POTION);
+        context.registerItemProperty(ITEM_MODEL_PROPERTY_LONG,
+                (ItemStack itemStack, ClientLevel clientLevel, LivingEntity livingEntity, int i) -> {
+                    if (!DistinguishedPotions.CONFIG.get(ClientConfig.class).dedicatedPotionBottles) return 0.0F;
+                    Potion potion = PotionNameHandler.getPotion(itemStack);
+                    return DistinguishedPotions.CONFIG.get(ClientConfig.class).longPotions.contains(potion) ? 1.0F :
+                            0.0F;
+                },
+                Items.POTION,
+                Items.SPLASH_POTION,
+                Items.LINGERING_POTION);
     }
 
     @Override
@@ -48,11 +71,5 @@ public class DistinguishedPotionsClient implements ClientModConstructor {
             }
             return false;
         }, Items.POTION, Items.SPLASH_POTION, Items.LINGERING_POTION, Items.TIPPED_ARROW);
-    }
-
-    @Override
-    public void onAddResourcePackFinders(PackRepositorySourcesContext context) {
-        // use a generated pack for the vanilla potion item models overrides, when included normally with mod resource at least on Forge the pack rarely ends up below vanilla breaking the overrides
-        context.addRepositorySource(PackResourcesHelper.buildClientPack(DistinguishedPotions.id("potion_overrides"), DynamicPackResources.create(DynamicModelProvider::new), false));
     }
 }
